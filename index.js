d282ad1b2d411bd994d6e5d78fd94c432af65c15
BlackLogo <77862659+sani495@users.noreply.github.com>
2022-08-21 13:37:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ code: `❄Pong! $pingms`
 //Ready Event when the bot is active
 bot.readyCommand({
     channel: "",
-    code: `$log[Ready on $userTag[$clientID]]`
+    code: `$log[Ready on $userTag[$clientID]]` // Will display on console when bot is online
 })
 
 /*
@@ -37,18 +37,3 @@ Be sure to install express to activate this --> npm i express
 */
 const loader = new aoijs.LoadCommands(bot)
  loader.load(bot.cmd,"./commands/") //This is a folder module for your commands to keep your commands organized.
-
-const {Panel} = require("@akarui/aoi.panel")
-
-const panel = new Panel({
-    username: "username",//username for logging in
-    password: "password",//password for logging in
-    secret: "aoijs",//session secret
-    port: 3000,//port on which website is hosted, Not required! Default 3000
-    bot: bot,//your aoi.js client
-    mainFile: "index.js",//Main file where code is running.Not required, default taken from package.json
-    commands: "commands"// folder name in which all the edit needing files are there.
-})
-panel.loadPanel()//Load The Panel
-
-panel.onError()//Will detect errors, and send it to aoi.panel's error page.
