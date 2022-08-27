033861f697cef066cc14368c9a93e0c4aef0e123
BlackLogo <77862659+sani495@users.noreply.github.com>
2022-08-27 11:18:02 -0700

    Create express.js

---
+++ Index/express.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+/* When using replit, you need a 24/7 hosting service./*
